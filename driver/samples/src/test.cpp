1dd82a4a36ff2ec183be9f7a31f376b2bfad3294
exto <extoxesses@gmail.com>
2018-12-26 10:49:20 +0100

    Successfully tested uinput mouse management

--- driver/samples/src/test.cpp
+++ driver/samples/src/test.cpp
@@ -1,22 +1,101 @@
-#include <xtfire/xtfire.h>
+// #include <xtfire/xtfire.h>
+// #include <iostream>
 
-#include <iostream>
+#include <linux/uinput.h>
 
-using namespace std;
-using namespace exto::emulation;
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+// using namespace std;
+// using namespace exto::emulation;
+
+//int xtFireTest(){
+//  try{
+//    cout << "Start emulation test..." << endl;
+//    XtFire emulator(0);
+//    emulator.start();
+//    cout << "Test ended!" << endl;
+
+//  } catch (invalid_argument* e){
+//    cout << " > Exception rised: " << endl;
+//    cout << e->what() << endl;
+//    return 0;
+//  }
+
+//  return 1;
+//}// xtFireTest
+
+/// =========
+
+void emit(int fd, int type, int code, int val) {
+   struct input_event ie;
+
+   ie.type = type;
+   ie.code = code;
+   ie.value = val;
+   /* timestamp values below are ignored */
+   ie.time.tv_sec = 0;
+   ie.time.tv_usec = 0;
+
+   write(fd, &ie, sizeof(ie));
+}
+
+
+void builtinTest(){
+  struct uinput_setup usetup;
+  int i = 50;
+
+  int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+
+  /* enable mouse button left and relative events */
+  ioctl(fd, UI_SET_EVBIT, EV_KEY);
+  ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
+
+  ioctl(fd, UI_SET_EVBIT, EV_REL);
+  ioctl(fd, UI_SET_RELBIT, REL_X);
+  ioctl(fd, UI_SET_RELBIT, REL_Y);
+
+  memset(&usetup, 0, sizeof(usetup));
+  usetup.id.bustype = BUS_USB;
+  usetup.id.vendor = 0x1234; /* sample vendor */
+  usetup.id.product = 0x5678; /* sample product */
+  strcpy(usetup.name, "Example device");
+
+  ioctl(fd, UI_DEV_SETUP, &usetup);
+  ioctl(fd, UI_DEV_CREATE);
+
+  /*
+   * On UI_DEV_CREATE the kernel will create the device node for this
+   * device. We are inserting a pause here so that userspace has time
+   * to detect, initialize the new device, and can start listening to
+   * the event, otherwise it will not notice the event we are about
+   * to send. This pause is only needed in our example code!
+   */
+  sleep(1);
+
+  /* Move the mouse diagonally, 5 units per axis */
+  while (i--) {
+     emit(fd, EV_REL, REL_X, 5);
+     emit(fd, EV_REL, REL_Y, 5);
+     emit(fd, EV_SYN, SYN_REPORT, 0);
+     usleep(15000);
+  }
+
+  /*
+   * Give userspace some time to read the events before we destroy the
+   * device with UI_DEV_DESTOY.
+   */
+  sleep(1);
+
+  ioctl(fd, UI_DEV_DESTROY);
+  close(fd);
+}// builtinTest
 
 int main(void){
-  try{
-    cout << "Start emulation test..." << endl;
-    XtFire emulator(0);
-    emulator.start();
-    cout << "Test ended!" << endl;
+  // return xtFireTest();
 
-  } catch (invalid_argument* e){
-    cout << " > Exception rised: " << endl;
-    cout << e->what() << endl;
-    return 0;
-  }
+  builtinTest();
 
   return 1;
 }// main
