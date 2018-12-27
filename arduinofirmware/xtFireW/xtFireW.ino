fcbd2af74c765872d8b9076b3e6a8d88a407cca5
exto <extoxesses@gmail.com>
2018-12-27 23:45:00 +0100

    Updated Arduino firmware to interface C64 Turbo controller with a common PC via USB. Added Fritzing scratch to notes C64 Turbo controller pinout

--- arduinofirmware/xtFireW/xtFireW.ino
+++ arduinofirmware/xtFireW/xtFireW.ino
@@ -15,14 +15,29 @@ unsigned long debounceTime = 0;
 void setup() {
   Serial.begin(serial_speed);
 
-  pinMode(a_button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(a_button), aButtonISR, RISING);
+  // pinMode(a_button, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(a_button), aButtonISR, RISING);
   
-  pinMode(b_button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(b_button), bButtonISR, RISING);
+  // pinMode(b_button, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(b_button), bButtonISR, RISING);
+
+  pinMode(30, INPUT);
+  pinMode(31, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
 }// setup
 
-void loop() { }// loop
+void loop() {
+  //*
+  int up = digitalRead(31);
+  int down = digitalRead(30);
+  int left = digitalRead(33);
+  int rigth = digitalRead(32);
+  // char msg[ ] = {up, ' ', down, ' ', left, ' ', rigth};
+  // String msg = String(up) + ' ' + String(down) + ' ' + String(left) + ' ' + String(rigth);
+  Serial.println(String(up));
+  //*/
+}// loop
 
 void aButtonISR() {
   serialSend(a_button);
